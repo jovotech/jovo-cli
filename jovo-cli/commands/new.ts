04da135d3e7a02353d6d1544acffd77944733df8
Jan Oberhauser <jan.oberhauser@gmail.com>
2019-01-15 17:42:38 +0100

    :bug: Fix issue that when "jovo new" got canceled because folder already existed it did go on anyway

--- jovo-cli/commands/new.ts
+++ jovo-cli/commands/new.ts
@@ -110,9 +110,10 @@ module.exports = (vorpal: Vorpal) => {
 			const frameworkVersion = args.options.v1 ? 1 : 2;
 			DEBUG = args.options.debug ? true : false;
 
+			let answers;
+
 			await project.init(frameworkVersion);
 
-			let p: Promise<void | JovoTaskContext> = Promise.resolve();
 			// @ts-ignore
 			const tasks = new Listr([], {
 				renderer: JovoCliRenderer,
@@ -125,39 +126,31 @@ module.exports = (vorpal: Vorpal) => {
 
 
 			if (!args.directory) {
-				p = p.then(() => {
-					return promptNewProject().then((answers) => {
-						args.directory = answers.directory;
-						if (!isValidProjectName(args.directory)) {
-							// Stop code execution
-							p = Promise.resolve();
-						} else {
-							return Promise.resolve();
-						}
-					});
-				});
+				answers = await promptNewProject();
+				args.directory = answers.directory;
+				if (!isValidProjectName(args.directory)) {
+					console.log(`The folder name "${args.directory}" is not valid`);
+					return;
+				}
 			}
 
 
-			// asks for approval when projectfolder exists
+			// asks for approval when project folder exists
 			if (args.directory && project.hasExistingProject(args.directory)) {
-				p = p.then(() => {
-					return promptOverwriteProject().then((answers) => {
-						if (answers.overwrite === ANSWER_CANCEL) {
-							// exit on cancel
-							p = Promise.resolve();
-						} else {
-							Utils.deleteFolderRecursive(path.join(process.cwd(), args.directory));
-							return Promise.resolve();
-						}
-					});
-				});
+				answers = await promptOverwriteProject();
+
+				if (answers.overwrite === ANSWER_CANCEL) {
+					// exit on cancel
+					return;
+				} else {
+					Utils.deleteFolderRecursive(path.join(process.cwd(), args.directory));
+				}
 			}
 
 			if (args.options.init) {
 				if (project.frameworkVersion !== 1) {
 					console.error('The "init" option got deprecated and is only available for Jovo Framework v1 projects.');
-					return Promise.resolve();
+					return;
 				}
 			}
 
@@ -176,123 +169,113 @@ module.exports = (vorpal: Vorpal) => {
 
 			// If not platform is given but needed get it from user
 			if (platformIsNeeded === true && config.types.length === 0) {
-				p = p.then(() => {
-					return promptForPlatform().then((answers) => {
-						config.types = [answers.platform];
-						console.log();
-						console.log();
-					});
-				});
+				answers = await promptForPlatform();
+				config.types = [answers.platform];
+				console.log();
+				console.log();
 			}
 
 			if (args.options.deploy) {
-				p = p.then(() => {
-					if (!args.options.build) {
-						console.log('Please use --build if you use --deploy');
-						p = Promise.resolve();
-					}
-					if (!args.options.init) {
-						console.log('Please use --init <platform> if you use --build');
-						p = Promise.resolve();
-					}
+				if (!args.options.build) {
+					console.log('Please use --build if you use --deploy');
+					return;
+				}
+				if (!args.options.init) {
+					console.log('Please use --init <platform> if you use --build');
+					return;
+				}
+			}
+
+			console.log('  I\'m setting everything up');
+			console.log();
+			_.merge(config, {
+				projectname: args.directory,
+				locales: project.getLocales(args.options.locale),
+				template: args.options.template || DEFAULT_TEMPLATE,
+				invocation: args.options.invocation,
+				endpoint: args.options.endpoint || DEFAULT_ENDPOINT,
+			});
+
+			// Apply platform specific config values
+			config.types.forEach((type: string) => {
+				const platform = Platforms.get(type);
+				_.merge(config, platform.getPlatformConfigValues(project, args.options));
+			});
+
+			project.setProjectPath(args.directory);
+			// @ts-ignore
+			config.template = config.template.replace('/', '-');
+			tasks.add({
+				// @ts-ignore
+				title: `Creating new directory /${chalk.white.bold(config.projectname)}`,
+				task: (ctx, task) => {
+					return project.createEmptyProject();
+				},
+			});
+
+			tasks.add({
+				title:
+				// @ts-ignore
+					`Downloading and extracting template ${chalk.white.bold(config.template)}`,
+				task: (ctx) => {
+					return project.downloadAndExtract(
+						ctx.projectname,
+						ctx.template,
+						ctx.locales[0]
+					)
+						.then(() => {
+							return project.updateModelLocale(ctx.locales[0]);
+						});
+				},
+			});
+
+			// init project
+			if (args.options.init || args.options.ff) {
+				// init project
+				tasks.add({
+					title: 'Initializing',
+					task: (ctx) => {
+						return new Listr([initTask()]);
+					},
 				});
 			}
 
-			p = p.then(() => {
-				console.log('  I\'m setting everything up');
-				console.log();
-				_.merge(config, {
-					projectname: args.directory,
-					locales: project.getLocales(args.options.locale),
-					template: args.options.template || DEFAULT_TEMPLATE,
-					invocation: args.options.invocation,
-					endpoint: args.options.endpoint || DEFAULT_ENDPOINT,
-				});
 
-				// Apply platform specific config values
-				config.types.forEach((type: string) => {
-					const platform = Platforms.get(type);
-					_.merge(config, platform.getPlatformConfigValues(project, args.options));
-				});
-
-				project.setProjectPath(args.directory);
-				// @ts-ignore
-				config.template = config.template.replace('/', '-');
-				tasks.add({
-					// @ts-ignore
-					title: `Creating new directory /${chalk.white.bold(config.projectname)}`,
-					task: (ctx, task) => {
-						return project.createEmptyProject();
-					},
-				});
-
-				tasks.add({
-					title:
-					// @ts-ignore
-						`Downloading and extracting template ${chalk.white.bold(config.template)}`,
-					task: (ctx) => {
-						return project.downloadAndExtract(
-							ctx.projectname,
-							ctx.template,
-							ctx.locales[0]
-						)
-							.then(() => {
-								return project.updateModelLocale(ctx.locales[0]);
-							});
-					},
-				});
-
-				// init project
-				if (args.options.init || args.options.ff) {
-					// init project
-					tasks.add({
-						title: 'Initializing',
-						task: (ctx) => {
-							return new Listr([initTask()]);
-						},
-					});
-				}
-
-
+			// build project
+			if (args.options.build || args.options.ff) {
 				// build project
-				if (args.options.build || args.options.ff) {
-					// build project
-					tasks.add({
-						title: 'Building',
-						task: (ctx) => {
-							return new Listr(buildTask(ctx));
-						},
-					});
-				}
-				// deploy project
-				if (args.options.deploy || args.options.ff) {
-					tasks.add({
-						title: 'Deploying',
-						task: (ctx) => {
-							return new Listr(deployTask(ctx));
-						},
-					});
-				}
-
-				// install npm dependencies
 				tasks.add({
-					title: 'Installing npm dependencies',
-					enabled: () => !args.options['skip-npminstall'],
-					task: () => project.runNpmInstall(),
+					title: 'Building',
+					task: (ctx) => {
+						return new Listr(buildTask(ctx));
+					},
 				});
-				return Promise.resolve(config);
+			}
+			// deploy project
+			if (args.options.deploy || args.options.ff) {
+				tasks.add({
+					title: 'Deploying',
+					task: (ctx) => {
+						return new Listr(deployTask(ctx));
+					},
+				});
+			}
+
+			// install npm dependencies
+			tasks.add({
+				title: 'Installing npm dependencies',
+				enabled: () => !args.options['skip-npminstall'],
+				task: () => project.runNpmInstall(),
 			});
 
-			return p.then((config) => {
-				return tasks.run(config).then(() => {
-					console.log();
-					console.log('  Installation completed.');
-					console.log();
-				}).catch((err) => {
-					if (DEBUG === true) {
-						console.error(err);
-					}
-				});
+			return tasks.run(config).then(() => {
+				console.log();
+				console.log('  Installation completed.');
+				console.log();
+			}).catch((err) => {
+				if (DEBUG === true) {
+					console.error(err);
+				}
 			});
 		});
 };
