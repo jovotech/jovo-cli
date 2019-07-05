a88a5d49e19813076a1fce7418e04023dff0c7b5
rubenaeg <email@aegerter.tech>
2019-07-05 11:14:00 +0200

    :recycle: Mior Refactoring

--- jovo-cli/test/Helpers.ts
+++ jovo-cli/test/Helpers.ts
@@ -41,7 +41,6 @@ export function runJovoCommand(command: string, parameters: string[], cwd: strin
 			reject(new Error(data.toString()));
 		});
 		child.stdout.on('data', (data) => {
-			console.log(data.toString());
 			if (waitText !== null) {
 				for (const text of waitText) {
 					if (data.toString().indexOf(text) > -1) {
