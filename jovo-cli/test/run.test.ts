008afe456f1a1257bfed9c56bd0fa47a833d5f62
rubenaeg <email@aegerter.tech>
2020-05-04 17:07:19 +0200

    :white_check_mark: Fix Unit Tests

--- jovo-cli/test/run.test.ts
+++ jovo-cli/test/run.test.ts
@@ -15,12 +15,12 @@ afterAll(() => {
 });
 
 describe('run', () => {
-  if (!process.env.ASK_PROFILE) {
-    console.log('Skipping because no ask profile found');
-    return;
-  }
-
   it('jovo run', async () => {
+    if (!process.env.ASK_PROFILE) {
+      console.log('Skipping because no ask profile found');
+      return;
+    }
+
     const projectName = 'helloworldRun';
     const projectFolder = path.join(tmpTestFolder, projectName);
 
@@ -32,6 +32,11 @@ describe('run', () => {
   }, 200000);
 
   it('jovo run --webhook-standalone', async () => {
+    if (!process.env.ASK_PROFILE) {
+      console.log('Skipping because no ask profile found');
+      return;
+    }
+
     const projectName = 'helloworldRun-standalone';
     const projectFolder = path.join(tmpTestFolder, projectName);
 
