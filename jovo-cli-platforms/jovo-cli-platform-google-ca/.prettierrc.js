8f5ef4497c9fcef92b4144c60c2f4581ba465299
rubenaeg <email@aegerter.tech>
2020-08-22 17:24:47 +0200

    🎨 Add .prettierrc.js

---
+++ jovo-cli-platforms/jovo-cli-platform-google-ca/.prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  printWidth: 100,
+  quoteProps: 'consistent',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
