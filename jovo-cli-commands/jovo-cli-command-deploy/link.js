5780b9c5b5e431a92b059b36eb300198afef2466
Ruben <email@aegerter.tech>
2021-03-18 13:24:53 +0100

    :hammer: Execute binaries for linking commands

--- jovo-cli-commands/jovo-cli-command-deploy/link.js
+++
@@ -1,7 +0,0 @@
-const { join } = require('path');
-const globalModules = require('global-modules');
-const symlinkDir = require('symlink-dir');
-
-(async () => {
-    await symlinkDir('.', join(globalModules, 'jovo-cli-command-deploy'));
-})();
