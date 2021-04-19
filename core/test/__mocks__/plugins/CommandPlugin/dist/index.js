a60731a2384eff8b05e4a643bcdab6de824f22aa
Ruben <email@aegerter.tech>
2021-04-19 18:42:21 +0200

    🚑️ Update __mocks__ for use with node@10.x

--- core/test/__mocks__/plugins/CommandPlugin/dist/index.js
+++ core/test/__mocks__/plugins/CommandPlugin/dist/index.js
@@ -1,8 +1,11 @@
 const { JovoCliPlugin } = require('../../../../../dist');
 
 class CommandPlugin extends JovoCliPlugin {
-  type = 'command';
-  id = 'commandPlugin';
+  constructor(config) {
+    super(config);
+    this.type = 'command';
+    this.id = 'commandPlugin';
+  }
 }
 
 module.exports = { CommandPlugin, default: CommandPlugin };
