981b49a8bb557211a723d27be771523a1f09e552
Ruben <email@aegerter.tech>
2021-04-20 10:14:59 +0200

    :sparkles: Implement deploy:code

--- commands/command-deploy/src/commands/deploy.code.ts
+++ commands/command-deploy/src/commands/deploy.code.ts
@@ -1,18 +1,48 @@
-import { flags, JovoCli, PluginCommand } from '@jovotech/cli-core';
+import * as Config from '@oclif/config';
+import {
+  checkForProjectDirectory,
+  CliArgs,
+  CliFlags,
+  Emitter,
+  flags,
+  JovoCli,
+  ParseContext,
+  PluginCommand,
+  PluginConfig,
+  PluginContext,
+  printSubHeadline,
+  TADA,
+} from '@jovotech/cli-core';
+
+import DeployCommand from '..';
 
 const jovo: JovoCli = JovoCli.getInstance();
 
+export type DeployCodeFlags = CliFlags<typeof DeployCode>;
+export type DeployCodeArgs = CliArgs<typeof DeployCode>;
+
+export interface DeployCodeContext extends PluginContext {
+  args: DeployCodeArgs;
+  flags: DeployCodeFlags;
+  target: string;
+}
+
+export interface ParseContextDeployCode extends ParseContext {
+  args: DeployCodeArgs;
+  flags: DeployCodeFlags;
+}
+
 export type DeployCodeEvents = 'before.deploy:code' | 'deploy:code' | 'after.deploy:code';
 
-export class DeployCode extends PluginCommand {
+export class DeployCode extends PluginCommand<DeployCodeEvents> {
   static id = 'deploy:code';
   static description = 'Deploys project code.';
-
   static examples: string[] = [
     'jovo deploy --locale en-US --platform alexaSkill --stage dev',
     'jovo deploy --target zip',
   ];
-
+  // Includes all available targets, which will be initialized on install().
+  static availableTargets: string[] = [];
   static flags = {
     locale: flags.string({
       char: 'l',
@@ -26,11 +56,6 @@ export class DeployCode extends PluginCommand {
         return jovo.getPlatforms();
       })(),
     }),
-    target: flags.string({
-      char: 't',
-      description: 'Target of build.',
-      // options: [TARGET_ALL, TARGET_INFO, TARGET_MODEL, TARGET_ZIP, ...deployTargets.getAllPluginTargets()],
-    }),
     stage: flags.string({
       description: 'Takes configuration from specified stage.',
     }),
@@ -40,5 +65,53 @@ export class DeployCode extends PluginCommand {
     }),
   };
 
-  async run(): Promise<void> {}
+  static args = [
+    <const>{
+      name: 'target',
+      description: 'Deploys.',
+      options: DeployCode.availableTargets,
+    },
+  ];
+
+  static async install(
+    plugin: DeployCommand,
+    emitter: Emitter<DeployCodeEvents>,
+    config: PluginConfig,
+  ): Promise<Config.Command.Plugin> {
+    // Override PluginCommand.install() to fill options for --platform.
+    this.availableTargets.push(...jovo.getPluginsWithType('target').map((plugin) => plugin.id));
+    return super.install(plugin, emitter, config);
+  }
+
+  async run(): Promise<void> {
+    checkForProjectDirectory();
+
+    const { args, flags }: Pick<ParseContextDeployCode, 'args' | 'flags'> = this.parse(DeployCode);
+
+    await this.$emitter.run('parse', { command: DeployCode.id, flags, args });
+
+    console.log();
+    console.log(`jovo deploy:code: ${DeployCode.description}`);
+    console.log(printSubHeadline('Learn more: https://jovo.tech/docs/cli/deploy-code\n'));
+
+    const context: DeployCodeContext = {
+      command: DeployCode.id,
+      platforms: jovo.getPlatforms(),
+      locales: flags.locale || jovo.$project!.getLocales(),
+      // ToDo: Configure deploy depending on target.
+      target: args.target,
+      // src: flags.src || jovo.$project!.getBuildDirectory(),
+      flags,
+      args,
+    };
+    jovo.setPluginContext(context);
+
+    await this.$emitter.run('before.deploy:code');
+    await this.$emitter.run('deploy:code');
+    await this.$emitter.run('after.deploy:code');
+
+    console.log();
+    console.log(`${TADA} Code deployment completed. ${TADA}`);
+    console.log();
+  }
 }
