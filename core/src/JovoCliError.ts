a0ce8a3b779aa9cc367cde26f1501bafac3e2bf6
Ruben <email@aegerter.tech>
2021-11-18 13:40:03 +0100

    :art: Add spacer to JovoCliError properties

--- core/src/JovoCliError.ts
+++ core/src/JovoCliError.ts
@@ -22,7 +22,7 @@ export class JovoCliError extends Error {
       prefix: ERROR_PREFIX,
       logLevel,
     });
-    Log.spacer();
+    Log.spacer(' ', 80, { prefix: ERROR_PREFIX, logLevel: LogLevel.Error });
   }
 
   static print(error: JovoCliError): void {
@@ -49,7 +49,6 @@ export class JovoCliError extends Error {
       this.addProperty('Stack', error.stack, LogLevel.Debug);
     }
 
-    Log.spacer(' ', 80, { prefix: ERROR_PREFIX, logLevel: LogLevel.Error });
     Log.info(
       chalk.grey(
         'If you think this is not on you, you can submit an issue here: https://github.com/jovotech/jovo-cli/issues.',
