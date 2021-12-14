7b5cba3f015505d17140f2c4937efaece21391da
ruben <email@aegerter.tech>
2021-12-14 11:02:31 +0100

    :children_crossing: Re-export everything from @jovotech/cli-core
    This enables users to use interfaces and classes of @jovotech/cli-core in hooks without adding another dependency

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -5,4 +5,4 @@ process.env.JOVO_CLI_RUNTIME = '1';
 process.env.JOVO_CLI_EXEC_PATH = dirname(__dirname);
 
 export { run } from '@oclif/command';
-export { ProjectConfig } from '@jovotech/cli-core';
+export * from '@jovotech/cli-core';
